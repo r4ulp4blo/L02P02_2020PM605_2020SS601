fffb7fd93eeab6ed1ad50a2535cdb6e57f5a75a1
r4ulp4blo <fokuto34@gmail.com>
2024-04-20 13:54:27 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2020PM605_2020SS601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2020PM605_2020SS601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ L02P02_2020PM605_2020SS601/Models/libreriaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2020PM605_2020SS601.Models
+{
+    public class libreriaDbContext : DbContext
+    {
+        public libreriaDbContext(DbContextOptions options): base(options) { }
+
+
+
+    }
+}
